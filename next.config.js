dd7db82f8651e7d31e158997e51934920052a419
usama7365 <usamaaamirsohail@gmail.com>
2023-08-31 12:25:53 -0700

    update URL

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    serverRuntimeConfig: {
+      // Set the base URL for the server
+      BASE_URL: process.env.NODE_ENV === 'production' ? 'https://wolkeyserver.onrender.com' : 'http://localhost:3000',
+    },
+    publicRuntimeConfig: {
+      // Expose the base URL to the client
+      BASE_URL: process.env.NODE_ENV === 'production' ? 'https://wolkeyclientside.onrender.com' : 'http://localhost:3000',
+    },
+  };
+  
