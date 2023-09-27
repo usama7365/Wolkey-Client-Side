dfed1a33e3c9261c6dcccdc8d7040573c671739f
usama7365 <usamaaamirsohail@gmail.com>
2023-09-27 16:25:42 -0700

    reolve build bugs

--- src/components/dataTable.js
+++ src/components/dataTable.js
@@ -1,70 +1,66 @@
-import React from "react";
-import BootstrapTable from "react-bootstrap-table-next";
-import cellEditFactory from "react-bootstrap-table2-editor";
-import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
-import paginationFactory from "react-bootstrap-table2-paginator"; // Import paginationFactory
+// import React from "react";
+// import BootstrapTable from "react-bootstrap-table-next";
+// import cellEditFactory from "react-bootstrap-table2-editor";
+// import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
+// import paginationFactory from "react-bootstrap-table2-paginator";
 
-const data = [
-  { products: "Product A", price: 50, date: "2023-09-20" },
-  { products: "Product B", price: 75, date: "2023-09-21" },
-  { products: "Product A", price: 50, date: "2023-09-20" },
-  { products: "Product B", price: 75, date: "2023-09-21" },
-  { products: "Product A", price: 50, date: "2023-09-20" },
-  { products: "Product B", price: 75, date: "2023-09-21" },
-  { products: "Product A", price: 50, date: "2023-09-20" },
-  { products: "Product B", price: 75, date: "2023-09-21" },
-  { products: "Product A", price: 50, date: "2023-09-20" },
-  { products: "Product B", price: 75, date: "2023-09-21" },
-  { products: "Product A", price: 50, date: "2023-09-20" },
-  { products: "Product B", price: 75, date: "2023-09-21" },
-];
 
-const columns = [
-  {
-    dataField: "id",
-  },
-  {
-    dataField: "products",
-    filter: textFilter({
-      placeholder: "", // Remove the label for this column
-    }),
-  },
-  {
-    dataField: "price",
-    text: "Price",
-    filter: textFilter({
-      label: false, // Remove the label for this column
-    }),
-  },
-  {
-    dataField: "date",
-    text: "Date",
-    filter: textFilter({
-      label: false, // Remove the label for this column
-    }),
-  },
-];
+// const data = [
+//   { products: "Product A", price: 50, date: "2023-09-20" },
+//   { products: "Product B", price: 75, date: "2023-09-21" },
+//   { products: "Product A", price: 50, date: "2023-09-20" },
+//   { products: "Product B", price: 75, date: "2023-09-21" },
+//   { products: "Product A", price: 50, date: "2023-09-20" },
+//   { products: "Product B", price: 75, date: "2023-09-21" },
+//   { products: "Product A", price: 50, date: "2023-09-20" },
+//   { products: "Product B", price: 75, date: "2023-09-21" },
+//   { products: "Product A", price: 50, date: "2023-09-20" },
+//   { products: "Product B", price: 75, date: "2023-09-21" },
+//   { products: "Product A", price: 50, date: "2023-09-20" },
+//   { products: "Product B", price: 75, date: "2023-09-21" },
+// ];
 
-const DataTable = () => {
-  const paginationOptions = {
-    sizePerPage: 10, 
-    pageStartIndex: 1, 
-    paginationSize: 5, 
-    hideSizePerPage: true, 
-  };
+// const columns = [
+//   {
+//     dataField: "id",
+//   },
+//   {
+//     dataField: "products",
+//     filter: textFilter({
+//       placeholder: "", // Remove the label for this column
+//     }),
+//   },
+//   {
+//     dataField: "price",
+//     text: "Price",
+//     editorRenderer: (editorProps, value, row, column, rowIndex, columnIndex) => {
+//  // Remove the label for this column
+//       return <input type="number" {...editorProps} />;
+//     },
+//   },
+//   {
+//     dataField: "date",
+//     text: "Date",
+//   },
+// ];
 
-  return (
-    <div>
-      <BootstrapTable
-        keyField="id"
-        data={data}
-        columns={columns}
-        cellEdit={cellEditFactory({ mode: "click", blurToSave: true })}
-        filter={filterFactory()}
-        pagination={paginationFactory(paginationOptions)} // Enable pagination
-      />
-    </div>
-  );
-};
+// const DataTable = () => {
+//   const paginationOptions = {
+//     sizePerPage: 10,
+//   };
 
-export default DataTable;
+//   return (
+//     <div>
+//     <BootstrapTable
+//       keyField="id"
+//       data={data}
+//       columns={columns}
+//       cellEdit={cellEditFactory({ mode: "click", blurToSave: true })}
+//       filter={filterFactory()}
+//       pagination={paginationFactory(paginationOptions)}
+//     />
+//   </div>
+//   );
+// };
+
+// export default DataTable;
